8b7b1560f1a9cb8e70a48891a997a170bb9955c5
byelladilipkumar <byelladilipkumar@gmail.com>
2024-11-03 19:38:20 +0530

    adding vercel link update

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 class Config:
     # Automatically generate a random secret key if one is not set
-    SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
+    SECRET_KEY = os.environ.get('SECRET_KEY', os.urandom(24))
 
     # PostgreSQL connection string
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'postgresql://postgres:dilip@localhost/pmdcs'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL',"postgresql://default:9CtJML4fvQhB@ep-winter-truth-a4ygmkt9.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
